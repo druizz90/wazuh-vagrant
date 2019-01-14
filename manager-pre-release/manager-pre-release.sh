09ac72487555edb2fe805395d3b1e8313bd0c658
druizz90 <demetrio.ruiz@wazuh.com>
2019-01-14 11:38:45 +0100

    Vagrantfile for configuring Wazuh manager (pre release) was added.

---
+++ manager-pre-release/manager-pre-release.sh
@@ -0,0 +1,47 @@
+#-----------------------------------
+# WAZUH MANAGER AND API INSTALLATION 
+#-----------------------------------
+
+# remove firewalld
+yum remove firewalld -y
+
+# install pip and boto3 (for AWS wodle)
+yum install epel-release -y
+yum -y update
+yum -y install python-pip
+
+# install net-tools
+yum install net-tools ntp -y
+
+# install git and zip
+yum install git zip -y
+
+# install wget
+yum install wget -y
+
+ntpdate -s time.nist.gov
+
+# Wazuh pre-release repository
+echo -e '[wazuh_pre_release]\ngpgcheck=1\ngpgkey=https://s3-us-west-1.amazonaws.com/packages-dev.wazuh.com/key/GPG-KEY-WAZUH\nenabled=1\nname=EL-$releasever - Wazuh\nbaseurl=https://s3-us-west-1.amazonaws.com/packages-dev.wazuh.com/pre-release/yum/\nprotect=1' | tee /etc/yum.repos.d/wazuh_pre.repo
+
+# install Wazuh manager
+yum install wazuh-manager -y
+
+# install Node.js
+curl --silent --location https://rpm.nodesource.com/setup_8.x | bash - 
+yum install nodejs -y 
+
+# install Wazuh API
+yum install wazuh-api -y
+
+# configure local_internal_options
+echo -e 'wazuh_modules.debug=1\nwazuh_database.sync_syscheck=1' >> /var/ossec/etc/local_internal_options.conf
+
+# enable Wazuh services
+systemctl daemon-reload
+systemctl enable wazuh-manager
+systemctl enable wazuh-api
+
+# run Wazuh manager and Wazuh API
+systemctl restart wazuh-manager
+systemctl restart wazuh-api
